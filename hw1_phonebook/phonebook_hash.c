b3660f36488f641bc0f90345a3ced6cf6bb51551
JayFeng0225 <jayfeng0225@gmail.com>
2016-03-27 14:20:53 +0800

    add hash version

---
+++ hw1_phonebook/phonebook_hash.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "phonebook_hash.h"
+
+/* FILL YOUR OWN IMPLEMENTATION HERE! */
+entry *findName(char lastname[], entry *pHead)
+{
+    /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+	}
+    return NULL;
+}
+
+entry *append(char lastName[], entry *e)
+{
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+    return e;
+}
+
+unsigned int hash(char *key){
+	unsigned int hashVal = 0;
+	while(*key != '\0')
+		hashVal = (hashVal << 5) + *key++;
+	return hashVal % 42737;
+}
+
+
